64c4e0ee78b9e38f56aec3b5295835bb395a9caf
song <scr0419@outlook.com>
2018-10-18 18:35:08 +0800

    增加审批校验信息

--- service/src/main/java/cn/thinkfree/service/approvalflow/impl/ApprovalFlowInstanceServiceImpl.java
+++ service/src/main/java/cn/thinkfree/service/approvalflow/impl/ApprovalFlowInstanceServiceImpl.java
@@ -5,8 +5,8 @@ import cn.thinkfree.database.mapper.ApprovalFlowInstanceMapper;
 import cn.thinkfree.database.model.*;
 import cn.thinkfree.database.vo.*;
 import cn.thinkfree.service.approvalflow.*;
-import cn.thinkfree.service.project.ProjectService;
 import cn.thinkfree.service.neworder.NewOrderUserService;
+import cn.thinkfree.service.project.ProjectService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,9 +45,9 @@ public class ApprovalFlowInstanceServiceImpl implements ApprovalFlowInstanceServ
     @Resource
     private RoleService roleService;
     @Resource
-    private ApprovalFlowNodeRoleService nodeRoleService;
-    @Resource
     private NewOrderUserService orderUserService;
+    @Resource
+    private ApprovalFlowNodeRoleService nodeRoleService;
 
     @Override
     public ApprovalFlowInstanceDetailVO detail(String num, String configNum, String projectNo, String userId, Integer scheduleSort, Integer scheduleVersion) {
