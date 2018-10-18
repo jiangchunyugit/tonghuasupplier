bdb4b49aca03ceff9b48cd311f5d955d19b35b13
zzzjiang <207782707@qq.com>
2018-10-18 18:34:00 +0800

    挪动调整包

--- service/src/main/java/cn/thinkfree/service/neworder/NewOrderUserService.java
+++ service/src/main/java/cn/thinkfree/service/neworder/NewOrderUserService.java
@@ -1,6 +1,10 @@
 package cn.thinkfree.service.neworder;
 
 import cn.thinkfree.database.model.OrderUser;
+import cn.thinkfree.database.vo.OrderConfirmationVO;
+import cn.thinkfree.database.vo.OrderDetailsVO;
+import cn.thinkfree.database.vo.ProjectOrderVO;
+import cn.thinkfree.database.vo.StageDetailsVO;
 
 import java.util.List;
 
@@ -19,4 +23,48 @@ public interface NewOrderUserService {
      * @return 项目用户关系
      */
     List<OrderUser> findByOrderNo(String orderNo);
+
+    /**
+     * @return
+     * @Author jiang
+     * @Description 分页查询项目派单
+     * @Date
+     * @Param
+     **/
+    List<ProjectOrderVO> queryProjectOrderByPage(ProjectOrderVO projectOrderVO, Integer pageNum, Integer pageSize);
+
+    /**
+     * @return
+     * @Author jiang
+     * @Description 查询项目派单总条数
+     * @Date
+     * @Param
+     **/
+    Integer queryProjectOrderCount(ProjectOrderVO projectOrderVO);
+
+    /**
+     * @return
+     * @Author jiang
+     * @Description 订单确认接口
+     * @Date
+     * @Param orderConfirmationVO
+     **/
+    Integer updateorderConfirmation(OrderConfirmationVO orderConfirmationVO);
+
+    /**
+     * @Author jiang
+     * @Description 查看订单详情
+     * @Date
+     * @Param
+     * @return
+     **/
+    OrderDetailsVO selectOrderDetails(String projectNo);
+    /**
+     * @Author jiang
+     * @Description 阶段展示
+     * @Date
+     * @Param
+     * @return
+     **/
+    List<StageDetailsVO> selectStageDetailsList(String projectNo);
 }
--- service/src/main/java/cn/thinkfree/service/neworder/NewOrderUserServiceImpl.java
+++ service/src/main/java/cn/thinkfree/service/neworder/NewOrderUserServiceImpl.java
@@ -1,8 +1,15 @@
 package cn.thinkfree.service.neworder;
 
-import cn.thinkfree.database.mapper.OrderUserMapper;
+import cn.thinkfree.database.mapper.*;
+import cn.thinkfree.database.model.DesignOrder;
+import cn.thinkfree.database.model.DesignOrderExample;
 import cn.thinkfree.database.model.OrderUser;
 import cn.thinkfree.database.model.OrderUserExample;
+import cn.thinkfree.database.vo.OrderConfirmationVO;
+import cn.thinkfree.database.vo.OrderDetailsVO;
+import cn.thinkfree.database.vo.ProjectOrderVO;
+import cn.thinkfree.database.vo.StageDetailsVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,5 +35,87 @@ public class NewOrderUserServiceImpl implements NewOrderUserService {
         OrderUserExample example = new OrderUserExample();
         example.createCriteria().andOrderNoEqualTo(orderNo);
         return orderUserMapper.selectByExample(example);
+    }
+    @Autowired(required = false)
+    private PreProjectGuideMapper preProjectGuideMapper;
+    @Autowired(required = false)
+    private DesignOrderMapper designOrderMapper;
+    @Autowired
+    private ProjectMapper projectMapper;
+    @Autowired
+    private ConstructionOrderMapper constructionOrderMapper;
+
+    /**
+     * @return
+     * @Author jiang
+     * @Description 分页查询项目派单
+     * @Date
+     * @Param
+     **/
+    @Override
+    public List<ProjectOrderVO> queryProjectOrderByPage(ProjectOrderVO projectOrderVO, Integer pageNum, Integer pageSize) {
+        projectOrderVO.setStatus(1);
+        return designOrderMapper.selectProjectOrderByPage(projectOrderVO, pageNum, pageSize);
+    }
+
+    /**
+     * @return
+     * @Author jiang
+     * @Description 查询项目派单总条数
+     * @Date
+     * @Param
+     **/
+    @Override
+    public Integer queryProjectOrderCount(ProjectOrderVO projectOrderVO) {
+        projectOrderVO.setStatus(1);
+        return designOrderMapper.selectProjectOrderCount(projectOrderVO);
+    }
+
+    /**
+     * @return
+     * @Author jiang
+     * @Description 订单确认接口
+     * @Date
+     * @Param orderConfirmationVO
+     **/
+    @Override
+    public Integer updateorderConfirmation(OrderConfirmationVO orderConfirmationVO) {
+        DesignOrder designOrder = new DesignOrder();
+        designOrder.setCompanyId(orderConfirmationVO.getCompanyId());
+        designOrder.setOrderStage(orderConfirmationVO.getOrderStage().intValue());
+
+        DesignOrderExample example = new DesignOrderExample();
+        example.createCriteria().andProjectNoEqualTo(orderConfirmationVO.getProjectNo());
+        return designOrderMapper.updateByExampleSelective(designOrder, example);
+    }
+
+    /**
+     * @Author jiang
+     * @Description 查看订单详情
+     * @Date
+     * @Param
+     * @return
+     **/
+    @Override
+    public OrderDetailsVO selectOrderDetails(String projectNo) {
+        OrderDetailsVO orderDetailsVO = new OrderDetailsVO();
+        orderDetailsVO.setProjectNo(projectNo);
+        orderDetailsVO.setStatus(1);
+
+        return projectMapper.selectOrderDetails(projectNo,orderDetailsVO.getStatus());
+    }
+    /**
+     * @Author jiang
+     * @Description 阶段展示
+     * @Date
+     * @Param
+     * @return
+     **/
+    @Override
+    public List<StageDetailsVO> selectStageDetailsList(String projectNo) {
+        StageDetailsVO stageDetailsVO = new StageDetailsVO();
+        stageDetailsVO.setType(3);//查询的是项目阶段 (1,设计订单 2,施工订单 3,项目)
+
+        return constructionOrderMapper.selectStageDetailsList(projectNo,stageDetailsVO.getType());
     }
 }
