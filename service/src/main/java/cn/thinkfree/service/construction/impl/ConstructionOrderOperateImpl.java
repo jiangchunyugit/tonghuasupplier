81d2b81fdfbaa4a91f3bda531f0f43e8224bbdd9
EastXu <172112222@qq.com>
2018-11-03 10:37:08 +0800

    commit 运营平台施工订单获取延期

--- service/src/main/java/cn/thinkfree/service/construction/impl/ConstructionOrderOperateImpl.java
+++ service/src/main/java/cn/thinkfree/service/construction/impl/ConstructionOrderOperateImpl.java
@@ -2,16 +2,14 @@ package cn.thinkfree.service.construction.impl;
 
 import cn.thinkfree.core.base.RespData;
 import cn.thinkfree.core.bundle.MyRespBundle;
-import cn.thinkfree.database.mapper.ConstructionOrderMapper;
-import cn.thinkfree.database.mapper.ProjectMapper;
-import cn.thinkfree.database.model.ConstructionOrder;
-import cn.thinkfree.database.model.ConstructionOrderExample;
-import cn.thinkfree.database.model.Project;
-import cn.thinkfree.database.model.ProjectExample;
+import cn.thinkfree.database.appvo.PersionVo;
+import cn.thinkfree.database.mapper.*;
+import cn.thinkfree.database.model.*;
 import cn.thinkfree.service.construction.CommonService;
 import cn.thinkfree.service.construction.ConstructionOrderOperate;
 import cn.thinkfree.service.construction.vo.ConstructionOrderListVo;
 import cn.thinkfree.service.construction.vo.ConstructionOrderManageVo;
+import cn.thinkfree.service.neworder.NewOrderUserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +18,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ConstructionOrderOperateImpl implements ConstructionOrderOperate {
@@ -32,6 +31,19 @@ public class ConstructionOrderOperateImpl implements ConstructionOrderOperate {
 
     @Autowired
     ProjectMapper projectMapper;
+
+    @Autowired
+    NewOrderUserService newOrderUserService;
+
+    @Autowired
+    EmployeeMsgMapper employeeMsgMapper;
+
+    @Autowired
+    CompanyInfoMapper companyInfoMapper;
+
+    @Autowired
+    ProjectBigSchedulingMapper projectBigSchedulingMapper;
+
 
     /**
      * 施工订单管理-列表
@@ -48,32 +60,60 @@ public class ConstructionOrderOperateImpl implements ConstructionOrderOperate {
         ConstructionOrderExample example = new ConstructionOrderExample();
         example.setOrderByClause("create_time DESC");
         List<ConstructionOrder> list = constructionOrderMapper.selectByExample(example);
-        List<ConstructionOrderListVo> listVo= new ArrayList<>();
+        List<ConstructionOrderListVo> listVo = new ArrayList<>();
 
-        for (ConstructionOrder constructionOrder:list){
+        for (ConstructionOrder constructionOrder : list) {
             ConstructionOrderListVo constructionOrderListVo = new ConstructionOrderListVo();
-            constructionOrderListVo.setAddress("所属地区");
-            constructionOrderListVo.setCompanyName("公司名称");
+
+            // 所属地区 & 项目地址 & 预约日期
+            List<Project> list1 = getProjectInfo(constructionOrder.getProjectNo());
+            constructionOrderListVo.setAddress(list1.get(0).getCity());
+            constructionOrderListVo.setAddressDetail(list1.get(0).getAddressDetail());
+            constructionOrderListVo.setAppointmentTime(list1.get(0).getCreateTime());
+
+            // 业主 & 手机号
+            PersionVo owner = getOwnerId(constructionOrder.getOrderNo());
+            constructionOrderListVo.setOwner(owner.getName());
+            constructionOrderListVo.setPhone(owner.getPhone());
+
+            // 项目经理 & 设计师
+            constructionOrderListVo.setProjectManager(getEmployeeInfo(constructionOrder.getCompanyId(), "CP"));
+            constructionOrderListVo.setDesignerName(getEmployeeInfo(constructionOrder.getCompanyId(), "CD"));
+
+            // 公司名称
+            List<CompanyInfo> list3 = getCompanyInfo(constructionOrder.getCompanyId());
+            constructionOrderListVo.setCompanyName(list3.get(0).getCompanyName());
+
+            // 订单编号 & 项目编号
             constructionOrderListVo.setOrderNo(constructionOrder.getOrderNo());
             constructionOrderListVo.setProjectNo(constructionOrder.getProjectNo());
-            constructionOrderListVo.setAppointmentTime(new Date());       // TODO 预约日期
-            constructionOrderListVo.setSignedTime(new Date());            // TODO 签约日期
-            constructionOrderListVo.setAddressDetail("项目地址");
-            constructionOrderListVo.setOwner("业主");
-            constructionOrderListVo.setPhone("手机号");
-            constructionOrderListVo.setReducedContractAmount(0);          // TODO 折后合同额10
-            constructionOrderListVo.setHavePaid(1);                       // TODO 已支付11
+            // 施工阶段
+            constructionOrderListVo.setConstructionProgress(getScheduill(constructionOrder.getConstructionStage()));
+
+            // TODO
             constructionOrderListVo.setOrderStage(constructionOrder.getOrderStage());
-            constructionOrderListVo.setConstructionProgress("施工进度");              // TODO 施工进度13
-            constructionOrderListVo.setCheckCondition(3);                           // TODO   最近验收情况14
+
+            // TODO 签约日期  sign_time
+            constructionOrderListVo.setSignedTime(new Date());
+            // TODO 折后合同额
+            constructionOrderListVo.setReducedContractAmount(0);
+            // TODO 已支付11
+            constructionOrderListVo.setHavePaid(1);
+
+
+            // TODO  ??????
+            // TODO   最近验收情况14
+            constructionOrderListVo.setCheckCondition(3);
 
             constructionOrderListVo.setDelayDays(15);                      // TODO   延期天数15
-            constructionOrderListVo.setProjectManager("项目经理");           //TODO 项目经理16
-            constructionOrderListVo.setDesignerName("设计师17");           //TODO 设计师17
+
 
             listVo.add(constructionOrderListVo);
         }
+
+
         pageInfo.setList(listVo);
+
 
         ConstructionOrderManageVo constructionOrderManageVo = new ConstructionOrderManageVo();
         constructionOrderManageVo.setCityList(commonService.getCityList());
@@ -89,19 +129,75 @@ public class ConstructionOrderOperateImpl implements ConstructionOrderOperate {
     }
 
     /**
-     *  查询项目信息
-     * @param projectNoList
+     * 查询项目信息
+     *
+     * @param projectNo
      * @return
      */
-    public List<Project> getProjectInfo (List<String> projectNoList){
+    public List<Project> getProjectInfo(String projectNo) {
         ProjectExample example = new ProjectExample();
-        example.createCriteria().andProjectNoIn(projectNoList);
+        example.createCriteria().andProjectNoEqualTo(projectNo);
         List<Project> list = projectMapper.selectByExample(example);
-        if (list.isEmpty()){
-            return null;
-        }else {
-            return list;
-        }
+        return list;
     }
+
+    /**
+     * 查询用户信息
+     *
+     * @param userId
+     * @return
+     */
+    public PersionVo getOwnerId(String userId) {
+        PersionVo owner = new PersionVo();
+        Map userName = newOrderUserService.getUserName(userId, "CC");
+        if(userName.isEmpty()){
+            owner.setPhone(null);
+            owner.setName(null);
+            return owner;
+        }
+        owner.setPhone(userName.get("phone").toString());
+        owner.setName(userName.get("nickName").toString());
+        return owner;
+    }
+
+    /**
+     * 设计师 & 项目经理
+     *
+     * @param companyId
+     * @return
+     */
+    public String getEmployeeInfo(String companyId, String role) {
+        EmployeeMsgExample example = new EmployeeMsgExample();
+        example.createCriteria().andCompanyIdEqualTo(companyId).andRoleCodeEqualTo(role).andEmployeeStateEqualTo(1);
+        List<EmployeeMsg> list = employeeMsgMapper.selectByExample(example);
+        return list.get(0).getRealName();
+    }
+
+    /**
+     * 公司名称
+     *
+     * @param companyId
+     * @return
+     */
+    public List<CompanyInfo> getCompanyInfo(String companyId) {
+        CompanyInfoExample example = new CompanyInfoExample();
+        example.createCriteria().andCompanyIdEqualTo(companyId);
+        List<CompanyInfo> list = companyInfoMapper.selectByExample(example);
+        return list;
+    }
+
+    /**
+     * 查询 施工阶段
+     *
+     * @param sort
+     * @return
+     */
+    public String getScheduill(int sort) {
+        ProjectBigSchedulingExample example = new ProjectBigSchedulingExample();
+        example.createCriteria().andSortEqualTo(sort);
+        List<ProjectBigScheduling> list = projectBigSchedulingMapper.selectByExample(example);
+        return list.get(0).getName();
+    }
+
 
 }
