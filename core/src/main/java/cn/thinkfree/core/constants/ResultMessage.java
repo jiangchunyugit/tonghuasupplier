9a0bb1203bc2eee5679db5ebc4761a940ff55f1d
xusonghui@juran.com.cn <602272186@qq.com>
2018-11-10 17:30:49 +0800

    修改操作错误，或者操作失败的时候的code值

--- core/src/main/java/cn/thinkfree/core/constants/ResultMessage.java
+++ core/src/main/java/cn/thinkfree/core/constants/ResultMessage.java
@@ -2,8 +2,8 @@ package cn.thinkfree.core.constants;
 
 public enum ResultMessage {
         SUCCESS(1000,"操作成功!"),
-        FAIL(400,"参数错误"),
-        ERROR(500,"操作失败!");
+        FAIL(9999,"参数错误"),
+        ERROR(9998,"操作失败!");
 
         public final Integer code ;
         public final String message;
