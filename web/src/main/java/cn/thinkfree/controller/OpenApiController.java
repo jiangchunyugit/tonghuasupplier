00926a1f94f8292a738c81b8131be1110420eefd
lastsmile <lishaodong201@sina.com>
2018-11-16 13:51:42 +0800

    源码丢失问题

--- web/src/main/java/cn/thinkfree/controller/OpenApiController.java
+++ web/src/main/java/cn/thinkfree/controller/OpenApiController.java
@@ -10,15 +10,17 @@ import cn.thinkfree.database.vo.EnterCompanyOrganizationVO;
 import cn.thinkfree.database.vo.ProjectQuotationVO;
 import cn.thinkfree.database.vo.SelectItem;
 import cn.thinkfree.service.branchcompany.BranchCompanyService;
+import cn.thinkfree.service.cache.RedisService;
 import cn.thinkfree.service.company.CompanyInfoService;
 import cn.thinkfree.service.project.ProjectService;
 import cn.thinkfree.service.sysMsg.SystemMessageService;
+import cn.thinkfree.service.user.UserService;
 import com.github.pagehelper.PageInfo;
-import com.sun.tools.javac.comp.Enter;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,6 +44,12 @@ public class OpenApiController extends AbsBaseController {
     CompanyInfoService companyInfoService;
 
     @Autowired
+    UserService userService;
+
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
     BranchCompanyService branchCompanyService;
 
     @ApiOperation(value = "APP模糊查询公司列表",notes = "默认30条数据")
@@ -52,6 +60,39 @@ public class OpenApiController extends AbsBaseController {
         List<SelectItem> items = companyInfoService.listCompanyByLikeName(name);
 
         return sendJsonData(ResultMessage.SUCCESS,items);
+    }
+
+
+    /**
+     * 忘记密码
+     * @param email
+     * @return
+     */
+    @ApiOperation(value = "忘记密码1",notes = "忘记密码第一步,验证邮箱")
+    @PostMapping("/forget")
+    @MyRespBody
+    public MyRespBundle<String> forgetPwd(String email){
+        String mes = userService.forgetPwd(email);
+        return sendSuccessMessage(mes);
+    }
+
+
+    /**
+     * 忘记密码 重置密码
+     * @param email
+     * @param pwd
+     * @param code
+     * @return
+     */
+    @ApiOperation(value = "重置密码",notes = "忘记密码第二部,重置密码")
+    @MyRespBody
+    @PostMapping("/forget/reset")
+    public MyRespBundle<String> resetPwd(String email,String pwd,String code){
+        if(StringUtils.isBlank(email) || StringUtils.isBlank(pwd)|| StringUtils.isBlank(code)){
+            return sendFailMessage("参数不对");
+        }
+        String mes = userService.updatePassWordOnForget(email,pwd,code);
+        return sendSuccessMessage(mes);
     }
 
 
@@ -102,5 +143,4 @@ public class OpenApiController extends AbsBaseController {
         List<CompanyInfo> companyInfos = branchCompanyService.getCompanyOrganizationByUser(userId);
         return sendJsonData(ResultMessage.SUCCESS,companyInfos);
     }
-
 }
