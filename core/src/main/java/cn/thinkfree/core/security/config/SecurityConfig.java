c0e17f0fb0e14d3198fe79df1f78b81c14e36148
lastsmile <lishaodong201@sina.com>
2018-11-21 17:05:50 +0800

    200与1000

--- core/src/main/java/cn/thinkfree/core/security/config/SecurityConfig.java
+++ core/src/main/java/cn/thinkfree/core/security/config/SecurityConfig.java
@@ -120,7 +120,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                         MyRespBundle<String> resp = new MyRespBundle<>();
                         resp.setTimestamp(Instant.now().toEpochMilli());
                         resp.setMessage("操作成功!");
-                        resp.setCode(200);
+                        resp.setCode(1000);
                         resp.setData("操作成功");
                         httpServletResponse.getWriter().write(new Gson().toJson(resp));
                     }
