576c888a183c1c97e828bb1dfb61849bb8d3deec
EastXu <172112222@qq.com>
2018-11-24 18:30:09 +0800

    commit 施工状态去掉尾款

--- service/src/main/java/cn/thinkfree/service/construction/ConstructionAndPayStateService.java
+++ service/src/main/java/cn/thinkfree/service/construction/ConstructionAndPayStateService.java
@@ -9,20 +9,6 @@ public interface ConstructionAndPayStateService {
 
     MyRespBundle<Boolean> isStagePay(String orderNo);
 
-    MyRespBundle<Boolean> isEndPay(String orderNo);
 
-    /**
-     * 开工报告
-     */
-    boolean isStartReport(String projectNo);
-
-    /**
-     * 阶段验收通过
-     */
-    boolean isStageComplete(String projectNo);
-
-    /**
-     * 竣工验收通过
-     */
-    boolean isBeComplete(String projectNo, String sort, String isEnd);
+    boolean isBeComplete(String projectNo, int sort);
 }
--- service/src/main/java/cn/thinkfree/service/construction/impl/ConstructionAndPayStateServiceImpl.java
+++ service/src/main/java/cn/thinkfree/service/construction/impl/ConstructionAndPayStateServiceImpl.java
@@ -9,16 +9,14 @@ import cn.thinkfree.database.mapper.ConstructionOrderPayMapper;
 import cn.thinkfree.database.mapper.ProjectBigSchedulingMapper;
 import cn.thinkfree.database.model.*;
 import cn.thinkfree.service.construction.CommonService;
+import cn.thinkfree.service.construction.ConstructionAndPayStateService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
-public class ConstructionAndPayStateServiceImpl {
+public class ConstructionAndPayStateServiceImpl implements ConstructionAndPayStateService {
 
     @Autowired
     CommonService commonService;
@@ -50,6 +48,7 @@ public class ConstructionAndPayStateServiceImpl {
      * 是否可以支付首款
      * 财务服务-刘博
      */
+    @Override
     public MyRespBundle<Boolean> isFirstPay(String orderNo) {
         if (StringUtils.isBlank(orderNo)) {
             return RespData.error(ResultMessage.ERROR.code, "订单编号不能为空");
@@ -68,6 +67,7 @@ public class ConstructionAndPayStateServiceImpl {
      * 是否可以阶段款
      * 财务服务-刘博
      */
+    @Override
     public MyRespBundle<Boolean> isStagePay(String orderNo) {
         if (StringUtils.isBlank(orderNo)) {
             return RespData.error(ResultMessage.ERROR.code, "订单编号不能为空");
@@ -84,83 +84,14 @@ public class ConstructionAndPayStateServiceImpl {
         }
     }
 
-    /**
-     * 是否可以付尾款
-     * 财务服务-刘博
-     */
-    public MyRespBundle<Boolean> isEndPay(String orderNo) {
-        if (StringUtils.isBlank(orderNo)) {
-            return RespData.error(ResultMessage.ERROR.code, "订单编号不能为空");
-        }
 
-        ConstructionOrderPayExample example = new ConstructionOrderPayExample();
-        example.createCriteria().andOrderNoEqualTo(orderNo);
-        List<ConstructionOrderPay> list =  constructionOrderPayMapper.selectByExample(example);
-        if (list.isEmpty()){
-            return RespData.success(false);
-        }
-        if ("C".equals(list.get(0).getIsEnd())){
-            return RespData.success(true);
-        }else {
-            return RespData.success(false);
-        }
-
-    }
-
-    /**
-     * 开工报告
-     * 内部服务-传让
-     */
-    public boolean isStartReport(String projectNo) {
-
-        //通过projectNo查询orderNo
-        ConstructionOrderExample example = new ConstructionOrderExample();
-        example.createCriteria().andProjectNoEqualTo(projectNo);
-        List<ConstructionOrder> constructionOrderList = constructionOrderMapper.selectByExample(example);
-        if (constructionOrderList.isEmpty()) {
-            return false;
-        }
-        String orderNo = constructionOrderList.get(0).getOrderNo();
-
-        // 首款完成-可开工报告
-        Integer stateCode = commonService.queryStateCodeByOrderNo(orderNo);
-        Integer stage = ConstructionStateEnumB.STATE_600.getState();
-        if (stateCode.equals(stage)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * 阶段验收通过
-     * 内部服务-传让
-     */
-    public boolean isStageComplete(String projectNo) {
-
-        //通过projectNo查询orderNo
-        ConstructionOrderExample example = new ConstructionOrderExample();
-        example.createCriteria().andProjectNoEqualTo(projectNo);
-        List<ConstructionOrder> constructionOrderList = constructionOrderMapper.selectByExample(example);
-        if (constructionOrderList.isEmpty()) {
-            return false;
-        }
-        String orderNo = constructionOrderList.get(0).getOrderNo();
-
-        Integer stateCode = commonService.queryStateCodeByOrderNo(orderNo);
-        Integer stage = ConstructionStateEnumB.STATE_630.getState();
-        if (stateCode.equals(stage)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     /**
      * 竣工验收通过
      * 内部服务-传让
      */
-    public boolean isBeComplete(String projectNo, String sort, String isEnd) {
+    @Override
+    public boolean isBeComplete(String projectNo, int sort) {
 
         //通过projectNo查询orderNo
         ConstructionOrderExample example = new ConstructionOrderExample();
@@ -169,40 +100,30 @@ public class ConstructionAndPayStateServiceImpl {
         if (constructionOrderList.isEmpty()) {
             return false;
         }
+        String orderNo = constructionOrderList.get(0).getOrderNo();
 
-        //通过sort 查找feeName
-        List<Integer> listNum = new ArrayList<>();
-        ProjectBigSchedulingExample example1 = new ProjectBigSchedulingExample();
-        example1.createCriteria().andSchemeNoEqualTo(constructionOrderList.get(0).getSchemeNo());
-        //查询施工阶段 排序
-        List<ProjectBigScheduling> schedulingList1 = projectBigSchedulingMapper.selectByExample(example1);
-        for (ProjectBigScheduling projectBigScheduling : schedulingList1) {
-            //查询是否竣工
-            listNum.add(projectBigScheduling.getSort());
-            Collections.sort(listNum);
-        }
-
-        //查询当前施工阶段
-        ProjectBigSchedulingExample example2 = new ProjectBigSchedulingExample();
-        example2.createCriteria().andSchemeNoEqualTo(constructionOrderList.get(0).getSchemeNo()).andSortEqualTo(Integer.parseInt(sort));
-        List<ProjectBigScheduling> schedulingList2 = projectBigSchedulingMapper.selectByExample(example2);
-        //开工报告 传值0
-        if (schedulingList2.size() > 0) {
-            for (ProjectBigScheduling projectBigScheduling : schedulingList2) {
-
-                // A开工报告 B阶段验收 C竣工验收
-                if (isEnd.equals("B")) {
-                    if (listNum.get(listNum.size() - 1).equals(projectBigScheduling.getSort())) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
+        /* sort==0 开工报告 */
+        if ("0".equals(sort)){
+            Integer stateCode = commonService.queryStateCodeByOrderNo(orderNo);
+            Integer stage = ConstructionStateEnumB.STATE_600.getState();
+            if (stateCode.equals(stage)) {
+                return true;
+            } else {
+                return false;
             }
-        } else {
-            return false;
         }
-        return false;
+        else {
+            sort +=1;
+            ConstructionOrderPayExample example1 = new ConstructionOrderPayExample();
+            example1.createCriteria().andSortEqualTo((short) sort).andIsEndEqualTo("pay");
+            List<ConstructionOrderPay> list = constructionOrderPayMapper.selectByExample(example1);
+            if (list.isEmpty()) {
+                return false;
+            }else {
+                return true;
+            }
+        }
+
     }
 
 }
