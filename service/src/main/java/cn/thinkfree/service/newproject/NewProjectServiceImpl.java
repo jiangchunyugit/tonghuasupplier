2d57392a7b04b8e305e9d47abd61ad0768082b99
gjm <17801118603@163.com>
2018-11-27 17:46:31 +0800

    获取施工资料接口  修改(优化提示)

--- service/src/main/java/cn/thinkfree/service/newproject/NewProjectServiceImpl.java
+++ service/src/main/java/cn/thinkfree/service/newproject/NewProjectServiceImpl.java
@@ -469,6 +469,9 @@ public class NewProjectServiceImpl implements NewProjectService {
         criteria.andStatusEqualTo(ProjectDataStatus.BASE_STATUS.getValue());
         criteria.andTypeEqualTo(ProjectDataStatus.QUOTATION_STATUS.getValue());
         List<ProjectData> projectDataList = projectDataMapper.selectByExample(example);
+        if(projectDataList.size()==ProjectDataStatus.INSERT_FAILD.getValue()){
+            return RespData.error("暂无施工资料");
+        }
         for (ProjectData projectData : projectDataList) {
             if (projectData.getIsConfirm() != null){
                 confirm = projectData.getIsConfirm();
