0e557bc3e167c02c6de9b78c6b7ad108ce6ba4ce
song <scr0419@outlook.com>
2018-11-28 15:12:24 +0800

    优化代码

--- web/src/main/java/cn/thinkfree/controller/ConstructionOrderController.java
+++ web/src/main/java/cn/thinkfree/controller/ConstructionOrderController.java
@@ -4,12 +4,11 @@ package cn.thinkfree.controller;
 import cn.thinkfree.core.annotation.MyRespBody;
 import cn.thinkfree.core.base.AbsBaseController;
 import cn.thinkfree.core.bundle.MyRespBundle;
+import cn.thinkfree.core.constants.ResultMessage;
 import cn.thinkfree.service.construction.*;
 import cn.thinkfree.service.construction.vo.*;
 import com.github.pagehelper.PageInfo;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,16 +43,16 @@ public class ConstructionOrderController extends AbsBaseController {
     OtherService otherService;
 
 
-
-
     @ApiOperation("运营平台接口（获取施工订单列表）---->孙宇专用")
     @MyRespBody
     @RequestMapping(value = "getOperateList", method = {RequestMethod.POST, RequestMethod.GET})
-    public MyRespBundle<ConstructionOrderCommonVo> getConstructionInfoList(@RequestParam @ApiParam(value = "页码",required = true) int pageNum,
-                                                                           @RequestParam @ApiParam(value = "每页条数",required = true) int pageSize,
-                                                                           @RequestParam(required = false) @ApiParam(value = "城市名称")  String cityName){
-
-        return constructionOrderOperate.getOrderList(pageNum,pageSize,cityName);
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum", value = "页码", defaultValue = "1"),
+            @ApiImplicitParam(name = "pageSize", value = "每页条数", defaultValue = "10"),
+            @ApiImplicitParam(name = "cityName", value = "城市名称")
+    })
+    public MyRespBundle<PageInfo<ConstructionOrderListVo>> getConstructionInfoList(@RequestParam int pageNum, @RequestParam int pageSize, @RequestParam(required = false) String cityName){
+        return sendJsonData(ResultMessage.SUCCESS, constructionOrderOperate.getOrderList(pageNum,pageSize,cityName));
     }
 
     @ApiOperation("运营平台接口（获取施工订单统计/城市）---->孙宇专用")
