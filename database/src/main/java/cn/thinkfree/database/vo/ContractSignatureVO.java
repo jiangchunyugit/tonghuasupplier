51468edc23f0c2571dccc0861dc580f2087be033
jiangchunyu <574577795@qq.com>
2018-12-05 16:39:51 +0800

    合同---入驻合同---合同签名上传

--- database/src/main/java/cn/thinkfree/database/vo/ContractSignatureVO.java
+++ database/src/main/java/cn/thinkfree/database/vo/ContractSignatureVO.java
@@ -4,15 +4,19 @@ import cn.thinkfree.database.model.ContractSignature;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @ApiModel(value = "合同签名信息")
 public class ContractSignatureVO {
 
     @ApiModelProperty(value="填写合同编号")
+    @NotBlank(message = "合同编号不可为空")
     private String contractNo;
 
     @ApiModelProperty(value="填写合同签名URLlist")
+    @NotEmpty(message = "合同地址不可为空")
     private List<ContractSignature> contractSignatures;
 
     public List<ContractSignature> getContractSignatures() {
