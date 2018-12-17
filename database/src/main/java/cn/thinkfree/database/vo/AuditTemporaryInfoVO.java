a3b51dd8bffbfa20cb897bf38bbb4edc7121e56f
ying007 <1598572441@qq.com>
2018-12-17 19:39:23 +0800

    资质变更修改

--- database/src/main/java/cn/thinkfree/database/vo/AuditTemporaryInfoVO.java
+++ database/src/main/java/cn/thinkfree/database/vo/AuditTemporaryInfoVO.java
@@ -16,6 +16,39 @@ public class AuditTemporaryInfoVO extends PcAuditTemporaryInfo{
     @ApiModelProperty("区名称")
     private String areaName;
 
+    @ApiModelProperty("省名称")
+    private String registerProvinceName;
+
+    @ApiModelProperty("市名称")
+    private String registerCityName;
+
+    @ApiModelProperty("区名称")
+    private String registerAreaName;
+
+    public String getRegisterProvinceName() {
+        return registerProvinceName;
+    }
+
+    public void setRegisterProvinceName(String registerProvinceName) {
+        this.registerProvinceName = registerProvinceName;
+    }
+
+    public String getRegisterCityName() {
+        return registerCityName;
+    }
+
+    public void setRegisterCityName(String registerCityName) {
+        this.registerCityName = registerCityName;
+    }
+
+    public String getRegisterAreaName() {
+        return registerAreaName;
+    }
+
+    public void setRegisterAreaName(String registerAreaName) {
+        this.registerAreaName = registerAreaName;
+    }
+
     public String getProvinceName() {
         return provinceName;
     }
