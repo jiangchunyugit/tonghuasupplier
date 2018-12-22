78d3e605f6fa7f34acf5bb38d03c839245997a37
xusonghui@juran.com.cn <602272186@qq.com>
2018-12-22 21:20:17 +0800

    修改/project/getDesignOrderData states=20

--- service/src/main/java/cn/thinkfree/service/newproject/NewProjectServiceImpl.java
+++ service/src/main/java/cn/thinkfree/service/newproject/NewProjectServiceImpl.java
@@ -1336,7 +1336,7 @@ public class NewProjectServiceImpl implements NewProjectService {
      * @return
      */
     @Override
-    public PageVo<List<DesignOrderVo>> getDesignOrderData(String designerId, String ownerMsg, String projectNo, int state, int pageIndex, int pageSize) {
+    public PageVo<List<DesignOrderVo>> getDesignOrderData(String designerId, String ownerMsg, String projectNo, List<Integer> states, int pageIndex, int pageSize) {
         if(pageIndex <= 0){
             pageIndex = 1;
         }
@@ -1364,8 +1364,11 @@ public class NewProjectServiceImpl implements NewProjectService {
             }
             projectNos.add(projectNo);
         }
-        long total = designerOrderMapper.countByDesignerId(designerId, ProjectDataStatus.BASE_STATUS.getValue(), projectNos, state);
-        List<DesignOrderVo> designOrderVos = designerOrderMapper.selectByDesignerId(designerId, ProjectDataStatus.BASE_STATUS.getValue(), projectNos, state, pageIndex - 1, pageSize);
+        if(states != null && states.isEmpty()){
+            states = null;
+        }
+        long total = designerOrderMapper.countByDesignerId(designerId, ProjectDataStatus.BASE_STATUS.getValue(), projectNos, states);
+        List<DesignOrderVo> designOrderVos = designerOrderMapper.selectByDesignerId(designerId, ProjectDataStatus.BASE_STATUS.getValue(), projectNos, states, pageIndex - 1, pageSize);
         if(designOrderVos == null || designOrderVos.isEmpty()){
             return PageVo.def(new ArrayList<>());
         }
--- web/src/main/java/cn/thinkfree/controller/AppProjectController.java
+++ web/src/main/java/cn/thinkfree/controller/AppProjectController.java
@@ -176,11 +176,11 @@ public class AppProjectController {
             @RequestParam("designerId") @ApiParam(name = "designerId", value = "设计师ID") String designerId,
             @RequestParam(value = "ownerMsg", required = false) @ApiParam(name = "ownerMsg", value = "业主信息，手机号/姓名") String ownerMsg,
             @RequestParam(value = "projectNo", required = false) @ApiParam(name = "projectNo", value = "项目编号") String projectNo,
-            @RequestParam(value = "state", required = false) @ApiParam(name = "state", value = "订单状态", defaultValue = "-1") int state,
+            @RequestParam(value = "states", required = false) @ApiParam(name = "states", value = "订单状态") List<Integer> states,
             @RequestParam(value = "pageIndex",required = false, defaultValue = "1") @ApiParam(name = "pageIndex", value = "第几页") int pageIndex,
             @RequestParam(value = "pageSize",required = false, defaultValue = "50") @ApiParam(name = "pageSize", value = "每页多少条")int pageSize) {
         try{
-            return RespData.success(newProjectService.getDesignOrderData(designerId, ownerMsg, projectNo, state, pageIndex, pageSize));
+            return RespData.success(newProjectService.getDesignOrderData(designerId, ownerMsg, projectNo, states, pageIndex, pageSize));
         }catch (Exception e){
             e.printStackTrace();
             return RespData.error(e.getMessage());
