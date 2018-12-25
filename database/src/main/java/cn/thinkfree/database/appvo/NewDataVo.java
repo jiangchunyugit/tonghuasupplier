2aa9c090f35c5c9bda064bc16ff49544c4d009e2
gjm <17801118603@163.com>
2018-12-25 19:20:57 +0800

    获取设计资料新接口 修改(添加资料是否确认)

--- database/src/main/java/cn/thinkfree/database/appvo/NewDataVo.java
+++ database/src/main/java/cn/thinkfree/database/appvo/NewDataVo.java
@@ -9,6 +9,10 @@ import lombok.Data;
 public class NewDataVo {
     @ApiModelProperty("量房资料详情")
     private String quantityDataJson;
+    @ApiModelProperty("量房资料是否确认  0,未确认 1,确认")
+    private Integer quantityDataIsConfirm;
     @ApiModelProperty("设计资料详情")
     private String designDataJson;
+    @ApiModelProperty("设计资料是否确认  0,未确认 1,确认")
+    private Integer designDataIsConfirm;
 }
