64bfd1dc14c299f37373b6a2ccf8c6dd01ff4a6e
jiangchunyugit <574577795@qq.com>
2019-10-07 17:43:41 +0800

    删除提交

---
+++ database/src/main/java/cn/tonghua/database/vo/FileResourceSEO.java
@@ -0,0 +1,5 @@
+package cn.tonghua.database.vo;
+
+public class FileResourceSEO extends AbsPageSearchCriteria {
+
+}
---
+++ database/src/main/java/cn/tonghua/database/vo/Severitys.java
@@ -0,0 +1,22 @@
+package cn.tonghua.database.vo;
+
+import javax.validation.Payload;
+
+/**
+ * 接口参数验证：注释分组--->1.添加分组，2.更新分组，3.查询分组
+ */
+public class Severitys {
+
+    /**
+     * 新增 新增时需要验证字段组
+     */
+    public interface Insert extends Payload{
+
+    }
+    /**
+     * 修改 修改时需要验证字段组
+     */
+    public interface Update extends Payload{
+
+    }
+}
