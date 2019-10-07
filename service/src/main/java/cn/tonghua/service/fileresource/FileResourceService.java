fcd7cf1dec52e2bf4e325cf1b3cacb0548607307
jiangchunyugit <574577795@qq.com>
2019-10-07 18:14:27 +0800

    删除提交

--- service/src/main/java/cn/tonghua/service/fileresource/FileResourceService.java
+++ service/src/main/java/cn/tonghua/service/fileresource/FileResourceService.java
@@ -1,21 +1,21 @@
-package cn.tonghua.service.fileresource;
-
-import cn.tonghua.database.model.FileResource;
-
-import java.util.List;
-
-public interface FileResourceService {
-
-    /**
-     * 返回全部资源
-     * @return
-     */
-    List<FileResource> fileResource();
-
-    /**
-     * 通过位置返回相应的资源
-     * @param area
-     * @return
-     */
-    FileResource fileResourceByArea(String area);
-}
+//package cn.tonghua.service.fileresource;
+//
+//import cn.tonghua.database.model.FileResource;
+//
+//import java.util.List;
+//
+//public interface FileResourceService {
+//
+//    /**
+//     * 返回全部资源
+//     * @return
+//     */
+//    List<FileResource> fileResource();
+//
+//    /**
+//     * 通过位置返回相应的资源
+//     * @param area
+//     * @return
+//     */
+//    FileResource fileResourceByArea(String area);
+//}
