cd148a3ec008972e37ea604b2c72da7217abfaa4
jiangchunyugit <574577795@qq.com>
2019-10-07 16:39:52 +0800

    删除提交

--- database/src/main/java/cn/thinkfree/database/pcvo/SchedulingCheckVo.java
+++
@@ -1,9 +0,0 @@
-package cn.thinkfree.database.pcvo;
-
-import io.swagger.annotations.ApiModel;
-import lombok.Data;
-
-@ApiModel("阶段验收实体")
-@Data
-public class SchedulingCheckVo {
-}
--- database/src/main/java/cn/thinkfree/database/vo/account/UserRoleSetSEO.java
+++
@@ -1,6 +0,0 @@
-package cn.thinkfree.database.vo.account;
-
-import cn.thinkfree.database.vo.AbsPageSearchCriteria;
-
-public class UserRoleSetSEO extends AbsPageSearchCriteria {
-}
